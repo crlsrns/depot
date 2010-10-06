ab76ddf4b909467d0e4769df52e560f077503c61
Carlos Arenas <carlos.arenas@gmail.com>
2010-10-06 13:20:35 -0500

    Added product validation (iteration b2)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ActiveRecord::Base
+  
+  validates :title,       :presence => true,
+                          :uniqueness => true
+  
+  validates :description, :presence => true
+  
+  validates :price,       :numericality => {:greater_than_or_equal_to => 0.01}
+  
+  validates :image_url,   :presence => true,
+                          :format => {
+                            :with	=> /\.(gif|jpg|png)$/i,
+                            :message => 'must be a URL for GIF, JPG or PNG image.'
+                          }
 end
