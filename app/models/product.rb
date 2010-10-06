c0a0cb2c3c32f4612918f1dadac7b870732ff9b9
Carlos Arenas <carlos.arenas@gmail.com>
2010-10-06 17:33:11 -0500

    Created catalog listing (iteration c1)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,17 @@
 class Product < ActiveRecord::Base
+  default_scope :order  => 'title'
   
-  validates :title,       :presence => true,
-                          :uniqueness => true
+  validates :title,       :presence     => true,
+                          :uniqueness   => true,
+                          :length       => { :minimum => 10 }
   
-  validates :description, :presence => true
+  validates :description, :presence     => true
   
   validates :price,       :numericality => {:greater_than_or_equal_to => 0.01}
   
-  validates :image_url,   :presence => true,
-                          :format => {
-                            :with	=> /\.(gif|jpg|png)$/i,
-                            :message => 'must be a URL for GIF, JPG or PNG image.'
+  validates :image_url,   :presence     => true,
+                          :format       => {
+                            :with	    => /\.(gif|jpg|png)$/i,
+                            :message  => 'must be a URL for GIF, JPG or PNG image.'
                           }
 end
